260d220c92cd2c1e02d4ea4c0fd2e1683f92f40e
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2024-01-07 15:41:49 -0500

    listen to backlog

--- src/App.svelte
+++ src/App.svelte
@@ -15,13 +15,17 @@
   import Transactions from "./components/txs/Transactions.svelte";
   import Events from "./components/events/Events.svelte";
   import About from "./components/about/About.svelte";
-  import { backlogInProgress, backlogSubmitted, minerEventReceived } from "./miner";
+  import {
+    backlogInProgress,
+    backlogSubmitted,
+    minerEventReceived,
+  } from "./miner";
   import { raise_error } from "./carpeError";
   import type { CarpeError } from "./carpeError";
   import { getEnv, responses, debugMode } from "./debug";
   import { routes } from "./routes";
   import "uikit/dist/css/uikit.min.css";
-  import { isCarpeInit} from "./accountActions";
+  import { isCarpeInit } from "./accountActions";
   import { getVersion } from "./version";
   import { carpeTick } from "./tick";
   import { init_preferences } from "./preferences";
@@ -29,21 +33,21 @@
   import RecoveryMode from "./components/layout/RecoveryMode.svelte";
   import MakeWhole from "./components/make-whole/MakeWhole.svelte";
   import { refreshUpstreamPeerStats } from "./networks";
-import { Level, logger } from "./logger";
-  
+  import { Level, logger } from "./logger";
+
   init_preferences();
- 
+
   let unlistenProofStart;
   let unlistenAck;
   let unlistenBacklogSuccess;
   let unlistenBacklogError;
+  let unlistenUpdater;
   let healthTick;
   let debug = false;
 
   onMount(async () => {
-
     logger(Level.Warn, "Webview is starting");
-    
+
     isCarpeInit();
 
     getEnv();
@@ -51,15 +55,14 @@ import { Level, logger } from "./logger";
     getVersion();
 
     // iterates through the list of peers in 0L.toml, and updates the statistics in preferences.json. So we don't need to test fullnodes on every transaction.
-    refreshUpstreamPeerStats()
-    .then(() => {
+    refreshUpstreamPeerStats().then(() => {
       carpeTick();
     });
 
     healthTick = setInterval(carpeTick, 30000); // do a healthcheck, this is async
 
-    debugMode.subscribe(b => debug = b);
-    
+    debugMode.subscribe((b) => (debug = b));
+
     ///// Backlog /////
     // Todo: Should this listener only be started in the miner view?
 
@@ -75,13 +78,10 @@ import { Level, logger } from "./logger";
       backlogInProgress.set(false);
       backlogSubmitted.set(false);
     });
-    
 
-    unlistenAck = await listen ("ack-backlog-request", (event: any) => {
+    unlistenAck = await listen("ack-backlog-request", (event: any) => {
       backlogInProgress.set(true);
     });
-
-    
 
     unlistenBacklogSuccess = await listen("backlog-success", (event: any) => {
       responses.set(event.payload);
@@ -91,14 +91,21 @@ import { Level, logger } from "./logger";
       carpeTick();
     });
 
-    unlistenBacklogError = await listen("backlog-error", (event: Event<CarpeError>) => {
-      // TODO: show an UX in the miner view for this type of error
-      
-      raise_error(event.payload, true, "listen(backlog-error)");
-      
-      backlogInProgress.set(false);
-      backlogSubmitted.set(false);
-    });
+    unlistenBacklogError = await listen(
+      "backlog-error",
+      (event: Event<CarpeError>) => {
+        // TODO: show an UX in the miner view for this type of error
+
+        raise_error(event.payload, true, "listen(backlog-error)");
+
+        backlogInProgress.set(false);
+        backlogSubmitted.set(false);
+      },
+    );
+
+    unlistenUpdater = await listen('tauri://update-status', (data: { payload: any }) => {
+      logger(Level.Warn, JSON.stringify(data?.payload));
+    })
   });
 
   onDestroy(() => {
@@ -106,14 +113,15 @@ import { Level, logger } from "./logger";
     unlistenAck();
     unlistenBacklogSuccess();
     unlistenBacklogError();
+    unlistenUpdater();
     clearInterval(healthTick);
-  })
+  });
 </script>
 
 <main class="uk-background-muted uk-height-viewport">
   <SearchingFullnodes />
-  <RecoveryMode/>
-  
+  <RecoveryMode />
+
   <div class="uk-container">
     <Router>
       <Nav />
@@ -127,7 +135,11 @@ import { Level, logger } from "./logger";
         />
         <Route path={routes.keygen} component={Keygen} primary={false} />
         <Route path={routes.miner} component={Miner} primary={false} />
-        <Route path={routes.transactions} component={Transactions} primary={false} />
+        <Route
+          path={routes.transactions}
+          component={Transactions}
+          primary={false}
+        />
         <Route path={routes.events} component={Events} primary={false} />
         <Route path={routes.settings} component={Settings} primary={false} />
         <Route path={routes.about} component={About} primary={false} />
@@ -138,12 +150,10 @@ import { Level, logger } from "./logger";
         <Route path={routes.swarm} component={Swarm} primary={false} />
 
         <!-- Show Debug Card Below -->
-        {#if debug }
-          <DebugCard/>
+        {#if debug}
+          <DebugCard />
         {/if}
-
-
       </div>
     </Router>
-  </div>  
+  </div>
 </main>
